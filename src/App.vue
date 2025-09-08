4c307bf6f91807c983de3b8df9cdff3b51a0d7b4
Mazin Al Saadi <maz1987in@gmail.com>
2025-09-08 21:42:10 +0400

    Initial

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<NcAppContent>
+		<div id="threedviewer">
+			<h1>Hello world!</h1>
+		</div>
+	</NcAppContent>
+</template>
+
+<script>
+import NcAppContent from '@nextcloud/vue/dist/Components/NcAppContent.js'
+
+export default {
+	name: 'App',
+	components: {
+		NcAppContent,
+	},
+}
+</script>
+
+<style scoped lang="scss">
+#threedviewer {
+	display: flex;
+	justify-content: center;
+	margin: 16px;
+}
+</style>
